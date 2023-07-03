e82a6fa29f837073b80e3b2a71b34951b1a6ff59
Kfmoi <faragk1@mcmaster.ca>
2023-07-03 18:20:06 -0400

    Regisitration fixed

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,37 +1,39 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { UserModel } from '../models/UserModel';
+import { UserModel } from '../models/UserModel.js';
 
 const router = express.Router();
 
 // Register
-router.post('/register', async (req, res) => {
-
-    const { name, email, password } = req.body;
-    
-    if(!name || !email || !password) {
-        return res.status(400).json({ message: "Please enter all required fields" });
+router.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Missing username and/or password" });
     }
-
-    const prevUser = UserModel.findOne({ username });
-
-    if(prevUser) {
-        return res.status(400).json({ message: "User already exists" });
+  
+    const prevUser = await UserModel.findOne({ username });
+  
+    if (prevUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
-
+  
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = new UserModel({
-        name,
-        email,
-        password: hashedPassword
+  
+    const user = new UserModel({
+      username,
+      password: hashedPassword,
     });
+  
+    await user.save();
+  
+    res.json({
+      status: `User ${username} registered!`,
+    });
+  });
+  
 
-    await newUser.save();
-
-    res.status(201).json({ message: "User registered successfully" });
-
-});
-
-export { router as userRouter};
+export { router as usersRouter};
