4d0a792180dfdf38e17440eddd54e04157f4d7b9
Kfmoi <faragk1@mcmaster.ca>
2023-07-03 18:21:32 -0400

    Imported the user router

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 // DB config
-mongoose.connect("mongodb+srv://kfmoi26:Kfmoi_1412003@taskmanager.j8rjpzt.mongodb.net/app?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGODB_URI);
 
 
 // API endpoints
