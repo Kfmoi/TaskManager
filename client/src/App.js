321db55cf38651e3380c728d31f37b234d6a800e
Kfmoi <faragk1@mcmaster.ca>
2023-07-03 19:13:36 -0400

    Imported the css

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Login, Register } from "./pages";
 import { Navbar } from "./components";
+import "./App.css";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Router>
+
         <Navbar />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-      </Router>
+
     </div>
+    </Router>
   );
 }
 
