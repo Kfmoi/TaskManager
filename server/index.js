3269e7aa1678b34eaf6d38b4bedf21a8a6e5969a
Kfmoi <faragk1@mcmaster.ca>
2023-07-03 17:59:07 -0400

    Add mongodb config

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// DB config
+mongoose.connect(process.env.MONGODB_URI);
+
 
 // API endpoints
 
+// Listener
 app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT}`));
