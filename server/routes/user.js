dc8c0632a766fb301210e5cf7dfda5d4722053b7
Kfmoi <faragk1@mcmaster.ca>
2023-07-03 18:25:15 -0400

    Created post request for login

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,39 +1,73 @@
-import express from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { UserModel } from '../models/UserModel.js';
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { UserModel } from "../models/UserModel.js";
 
 const router = express.Router();
 
 // Register
 router.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-  
-    if (!username || !password) {
-      return res
-        .status(400)
-        .json({ message: "Missing username and/or password" });
-    }
-  
-    const prevUser = await UserModel.findOne({ username });
-  
-    if (prevUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-  
-    const hashedPassword = await bcrypt.hash(password, 10);
-  
-    const user = new UserModel({
-      username,
-      password: hashedPassword,
-    });
-  
-    await user.save();
-  
-    res.json({
-      status: `User ${username} registered!`,
-    });
-  });
-  
+  const { username, password } = req.body;
 
-export { router as usersRouter};
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Missing username and/or password" });
+  }
+
+  const prevUser = await UserModel.findOne({ username });
+
+  if (prevUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new UserModel({
+    username,
+    password: hashedPassword,
+  });
+
+  await user.save();
+
+  res.json({
+    status: `User ${username} registered!`,
+  });
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Missing username and/or password" });
+  }
+
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.status(400).json({ message: "User does not exist" });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign(
+    { username: user.username, id: user._id },
+    process.env.JWT_SECRET
+  );
+
+  res.json({
+    status: `User ${username} logged in!`,
+    token,
+  });
+});
+
+
+
+export { router as usersRouter };
