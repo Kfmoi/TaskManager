3ce679843d0063095da2cb6b02b1b1705fa30383
Kfmoi <faragk1@mcmaster.ca>
2023-07-04 00:33:21 -0400

    Created a route for create task

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Login, Register, Home } from "./pages";
+import { Login, Register, Home, CreateTask } from "./pages";
 import { Navbar } from "./components";
 import "./App.css";
 
@@ -14,6 +14,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Home />} />
+          <Route path="/create-task" element={<CreateTask />} />
+
         </Routes>
 
     </div>
