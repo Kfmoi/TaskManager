cafcbc38908a7c36a47a04799992114cd2a8a5a1
Kfmoi <faragk1@mcmaster.ca>
2023-07-04 23:56:47 -0400

    Added logos for the edit and delete buttons

--- client/src/components/task/Task.js
+++ client/src/components/task/Task.js
@@ -3,6 +3,9 @@ import "./task.css";
 import PendingIcon from "../../assets/pending.png";
 import DelayedIcon from "../../assets/delayed.png";
 import CompletedIcon from "../../assets/completed.png";
+import DeleteIcon from "../../assets/delete.png"
+import EditIcon from "../../assets/edit.png"
+
 
 const Task = ({ title, desc, status, edit, del, taskId }) => {
   return (
@@ -10,8 +13,12 @@ const Task = ({ title, desc, status, edit, del, taskId }) => {
       <div className="task-top">
         <h1 className="task-title">{title}</h1>
         <div className="task-top-buttons">
-          <button onClick={edit}>Edit</button>
-          <button onClick={del}>Delete</button>
+          <button onClick={edit}>
+            <img src={EditIcon} alt="edit" className="modify" />
+          </button>
+          <button onClick={del}>
+          <img src={DeleteIcon} alt="delete" className="modify" />
+          </button>
         </div>
       </div>
       <div className="task-bottom">
